70147f67d66c95f61d6a6b7ac7265d0d62a67825
Trotyl Yu <trotyl@qq.com>
2018-10-24 13:36:26 +0800

    compare export declaration and expression

---
+++ export-declaration/bar.js
@@ -0,0 +1,5 @@
+import { foo } from './foo.js'
+
+export function bar(x) {
+  return foo(x * 2)
+}
---
+++ export-declaration/foo.js
@@ -0,0 +1,7 @@
+import { bar } from './bar.js'
+
+console.log(bar(42))
+
+export function foo(x) {
+  return x + 1
+}
